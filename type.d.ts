2197435173ad3fbe7892dcc94a2fbfea351be336
Sukoshi <sukoshi@hanyouweb.com>
2025-03-28 15:49:53 +0800

    feat: update

--- type.d.ts
+++ type.d.ts
@@ -4,21 +4,9 @@ interface Photo {
   color: string;
   is_sticky?: boolean;
   clip?: string;
-  exif?: string;
   thumb_img?: string;
   live_video?: string;
-  location_map?: { [key: string]: any };
+  exif?: { [key: string]: any };
   location_info?: { [key: string]: any };
-}
-
-interface PreviewPhoto {
-  el: HTMLElement;
-  index: number;
-  name?: string;
-  origin?: string;
-  clip?: string;
-  color?: string;
-  exif?: Record<string, any>;
-  live_video?: string;
   location_map?: string;
 }
